f87693701621f7d574eddba5e6ec783f835e65ac
Brian Jablonsky <bjablonsky@users.noreply.github.com>
2019-10-01 12:46:26 -0400

    Print message 5 times

---
+++ DemoApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Set the message
+            String message = "Hello NYC .NET Hacktoberfest!";
+
+            // Print out the message 5 times
+            for(int i = 0; i <= 5; i++) {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
